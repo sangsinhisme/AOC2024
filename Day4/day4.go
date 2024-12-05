cbaeeb6b6c65f7e8ec2a2f0a3a77698ba9f2b8f9
Nguyen Sang Sinh <sangsinh789@gmail.com>
2024-12-05 09:28:13 +0700

    ajourter4(jeudi 5, decembre): ceres search

--- Day4/day4.go
+++ Day4/day4.go
@@ -3,52 +3,98 @@ package Day4
 import (
 	"AOD2024/Utils"
 	"fmt"
-	"math"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
 const day = 4
 
 func XMAS(n, m int, graph [][]int32) int {
-	var helper func(i, j int, visited map[[2]int]int, xmas string) int
-	helper = func(i, j int, visited map[[2]int]int, xmas string) int {
-		ans := 0
-		if strings.HasPrefix("XMAS", xmas) {
-			if xmas == "XMAS" {
-				return 1
+	var helper func(i, j, ii, jj int) bool
+	helper = func(i, j, ii, jj int) bool {
+		for _, elem := range "XMAS" {
+			if i < 0 || i > n-1 || j < 0 || j > m-1 || graph[i][j] != elem {
+				return false
 			}
-			visited[[2]int{i, j}] = 1
-			for x := -1; x <= 1; x++ {
-				for y := -1; y <= 1; y++ {
-					nextCurrX, nextCurrY := i+x, j+y
-					if nextCurrX > -1 && nextCurrX < n && nextCurrY > -1 && nextCurrY < m && visited[[2]int{nextCurrX, nextCurrY}] != 1 {
-						nextString := xmas + string(graph[nextCurrX][nextCurrY])
-						if strings.HasPrefix("XMAS", nextString) {
-							fmt.Println(i, j, nextCurrX, nextCurrY, nextString)
-							visited[[2]int{nextCurrX, nextCurrY}] = 1
-							ans += helper(nextCurrX, nextCurrY, visited, nextString)
-							visited[[2]int{nextCurrX, nextCurrY}] = 0
-						}
-
-					}
-				}
+			i = i + ii
+			j = j + jj
+		}
+		return true
+	}
+	direct := make([][2]int, 0)
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			if i != 0 || j != 0 {
+				direct = append(direct, [2]int{i, j})
 			}
 		}
-		return ans
 	}
-
 	ans := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			if graph[i][j] == 'X' {
-				ans += helper(i, j, map[[2]int]int{}, "X")
+			for _, d := range direct {
+				if helper(i, j, d[0], d[1]) {
+					ans++
+				}
 			}
 		}
 	}
 	return ans
 }
+
+func MAS(n, m int, graph [][]int32) int {
+	direct := [][3]int{
+		{-1, -1},
+		{1, 1},
+		{-1, 1},
+		{1, -1},
+	}
+
+	var helper func(i, j int) bool
+	helper = func(i, j int) bool {
+		idxMap := make(map[int]int32)
+		for idx, elem := range direct {
+			nextI, nextJ := i+elem[0], j+elem[1]
+			if nextI > -1 && nextI < n && nextJ > -1 && nextJ < m {
+				idxMap[idx] = graph[nextI][nextJ]
+			}
+		}
+		if len(idxMap) != 4 {
+			return false
+		}
+		mCountFirst := 0
+		sCountFirst := 0
+		mCountSecond := 0
+		sCountSecond := 0
+
+		for i, val := range idxMap {
+			switch val {
+			case 'M':
+				if i < 2 {
+					mCountFirst++
+				} else {
+					mCountSecond++
+				}
+			case 'S':
+				if i < 2 {
+					sCountFirst++
+				} else {
+					sCountSecond++
+				}
+			}
+		}
+
+		return mCountFirst == 1 && sCountFirst == 1 && mCountSecond == 1 && sCountSecond == 1
+	}
+	ans := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if graph[i][j] == 'A' && helper(i, j) {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
 func Part1(submit bool) {
 	lines, err := Utils.ReadFileLines(fmt.Sprintf("Day%v/sample.txt", day))
 	if submit {
@@ -72,9 +118,9 @@ func Part1(submit bool) {
 	}
 	ans := XMAS(n, m, graph)
 	fmt.Println(Utils.Yellow + fmt.Sprintf("Answers this part: %v. Let's submit this problem.", ans) + Utils.Reset)
-	//if submit {
-	//	Utils.Submit(day, 1, ans)
-	//}
+	if submit {
+		Utils.Submit(day, 1, ans)
+	}
 }
 
 func Part2(submit bool) {
@@ -87,56 +133,20 @@ func Part2(submit bool) {
 		}
 		lines, err = Utils.ReadFileLines(fmt.Sprintf("Input/%v.txt", day))
 	}
-
-	ans := 0
-	lastStatus := true
-	for _, line := range lines {
-		re := regexp.MustCompile(`mul\((?P<I1>\d+),(?P<I2>\d+)\)`)
-		matches := re.FindAllStringSubmatchIndex(line, -1)
-
-		mapDoDont := make(map[int]bool)
-		re = regexp.MustCompile(`do\(\)`)
-		do := re.FindAllStringSubmatchIndex(line, -1)
-
-		re = regexp.MustCompile(`don't\(\)`)
-		dont := re.FindAllStringSubmatchIndex(line, -1)
-
-		for _, elem := range do {
-			mapDoDont[elem[0]] = true
-		}
-		for _, elem := range dont {
-			mapDoDont[elem[0]] = false
-		}
-
-		for _, match := range matches {
-			lastStatus = fineNearstDoDont(mapDoDont, match[0], lastStatus)
-			if lastStatus {
-				num1, _ := strconv.Atoi(line[match[2]:match[3]])
-				num2, _ := strconv.Atoi(line[match[4]:match[5]])
-				ans += num1 * num2
-			}
+	n := len(lines)
+	m := len(lines[0])
+	graph := make([][]int32, n)
+	for i := range n {
+		graph[i] = make([]int32, m)
+	}
+	for i, line := range lines {
+		for j, char := range line {
+			graph[i][j] = char
 		}
 	}
-
+	ans := MAS(n, m, graph)
 	fmt.Println(Utils.Yellow + fmt.Sprintf("Answers this part: %v. Let's submit this problem.", ans) + Utils.Reset)
 	if submit {
 		Utils.Submit(day, 2, ans)
 	}
-}
-
-func fineNearstDoDont(doDont map[int]bool, idx int, lastStatus bool) bool {
-	nearst := math.MaxInt
-	status := true
-	for elem := range doDont {
-		if elem < idx {
-			if idx-elem < nearst {
-				nearst = idx - elem
-				status = doDont[elem]
-			}
-		}
-	}
-	if nearst == math.MaxInt {
-		return lastStatus
-	}
-	return status
 }
--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"AOD2024/Day4"
-	"fmt"
-	"strings"
 )
 
 func main() {
-	Day4.Part1(false)
-	//Day4.Part2(false)
-
-	fmt.Println(strings.HasPrefix("XMAS", "X"))
+	Day4.Part1(true)
+	Day4.Part2(true)
 }
